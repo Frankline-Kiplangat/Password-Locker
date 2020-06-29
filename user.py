d7e6c73499c334d2f1b6d9ef62b251dc4d3d861b
Frank <kipfrankline@gmail.com>
2020-06-30 00:04:47 +0300

    Add user class function that can save, delete and find user

---
+++ user.py
@@ -0,0 +1,30 @@
+class User:
+
+    user_list = []
+
+    def __init__(self, username, password):
+        '''
+        Function to save user credentials
+        '''
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        User.user_list.append(self)
+
+
+    def delete_user(self):
+        '''
+        delete a user account
+        '''
+        User.user_list.remove(self)
+
+
+    @classmethod
+    def find_user(cls, username):
+        '''
+        find username using search terms
+        '''
+        for user in cls.user_list:
+            if user.username == username:
+                return  user
