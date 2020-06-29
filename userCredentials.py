d248ffef3a62ee29c73c5bb337af143c51f05a23
Frank <kipfrankline@gmail.com>
2020-06-29 23:45:43 +0300

    Add function that saves and delete user Credentials

---
+++ userCredentials.py
@@ -0,0 +1,33 @@
+import pyperclip
+class UserCredentials:
+    """
+    class that creates instance of user accounts
+    """
+    userCred_list = []
+
+    # credential list
+
+    def __init__(self, account, email, password):
+
+        self.account = account
+        self.email = email
+        self.password = password
+
+
+        # saving credentials
+    
+    def save_userCred(self):
+        """
+        credentials list
+        """
+        UserCredentials.userCred_list.append(self)
+
+        # deleting credentials
+
+    def delete_userCred(self):
+        """
+        delete  user credentials
+        """
+        UserCredentials.userCred_list.remove(self)
+
+        # searching for credentials
