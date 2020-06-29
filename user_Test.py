3c6a8d951a272d70f9218a6c320f9fa223267861
Frank <kipfrankline@gmail.com>
2020-06-30 00:15:32 +0300

    Run second test

--- user_Test.py
+++ user_Test.py
@@ -16,3 +16,23 @@ class TestUser(unittest.TestCase):
         User.user_list = []
 
         #first test- initialization
+  
+    def test__init(self):
+        '''
+        check if class is initialiazing as expected
+        '''
+        self.assertEqual(self.new_user.username, "Frankline-Kiplangat")
+        self.assertEqual(self.new_user.password, "frankdux@1234")
+
+
+
+    def test_save_user(self):
+        '''
+        check whether the user information can be saved 
+        in the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+        #run second test to check if we can save multiple users
+             
