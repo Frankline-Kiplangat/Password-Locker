b1b6cd9b1226f9beb8d492cdf680133fae0d01c3
Frank <kipfrankline@gmail.com>
2020-06-29 23:47:56 +0300

    Add class methods

--- userCredentials.py
+++ userCredentials.py
@@ -31,3 +31,39 @@ class UserCredentials:
         UserCredentials.userCred_list.remove(self)
 
         # searching for credentials
+
+    @classmethod
+    def find_account(cls, account):
+        """
+        search for accounts
+        """
+        for userCred in cls.userCred_list:
+            if userCred.account == account:
+                return userCred
+
+        # confirming details
+
+    @classmethod
+    def userCred_exists(cls, account):
+        """
+        confirm if a class exits
+        """
+        for userCred in cls.userCred_list:
+            if userCred.account == account:
+                return True
+            return False
+    
+    #Display user Credentials
+
+    @classmethod
+    def display_userCred(cls):
+        """
+        Function that returns all credentials
+        """
+        return cls.userCred_list
+
+    @classmethod
+    def copy_password(cls, password):
+            find_account = UserCredentials.find_account(password)
+            pyperclip.copy(find_account.password)
+
